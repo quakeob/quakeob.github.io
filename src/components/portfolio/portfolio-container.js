13fda72213bb5a70f44344e77fbabde9499a1513
Jake davis <jakejumped@gmail.com>
2022-01-26 15:26:09 -0700

    update 1/16/22

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -12,15 +12,10 @@ export default class PortfolioContainer extends Component {
     this.state = {
       pageTitle: "Welcome to my portfolio",
       isLoading: false,
-      data: [
-        {title:"Ice Castles", category: "Tourism", slug: 'ice-castles' },
-        {title:"Vail Resorts", category: "Tourism", slug: 'vail-resorts' },
-        {title:"Victory Ranch", category: "Golf", slug: 'victory-ranch' }
-      ]
+      data: []
     };
    
     this.handleFilter = this.handleFilter.bind(this);
-    this.getPortfolioItems = this.getPortfolioItems.bind(this);
   }
 
   handleFilter(filter) {
@@ -36,6 +31,9 @@ export default class PortfolioContainer extends Component {
       .get("https://jakedavis.devcamp.space/portfolio/portfolio_items")
       .then(response => {
         console.log("response data", response);
+        this.setState({
+          data: response.data.portfolio_items
+        })
       })
       .catch(error => {
         console.log(error);
@@ -44,17 +42,20 @@ export default class PortfolioContainer extends Component {
 
   PortfolioItems() {
     return this.state.data.map(item => {
-      return <PortfolioItem title={item.title} url={"google.com"} slug={item.slug} />;
+      return <PortfolioItem key={item.id} title={item.name} url={item.url} slug={item.id} />;
     });
   }
 
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
 
   render() {
     if (this.state.isLoading) {
       return <div>Loading...</div>;
     }
 
-    this.getPortfolioItems();
+
 
     return (
       <div>
